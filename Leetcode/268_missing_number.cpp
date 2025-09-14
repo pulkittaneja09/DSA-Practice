5acc0c2175cadcf1a4d3a3db513fe54d90826cd5
pulkittaneja09 <pulkittaneja0509@gmail.com>
2025-09-14 23:57:23 +0530

    Add solution: 268 Missing Number

---
+++ Leetcode/268_missing_number.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        long long expected = (long long)n * (n + 1) / 2; // sum from 0..n
+        long long actual = 0;
+        for (int num : nums) {
+            actual += num;
+        }
+        return (int)(expected - actual);
+    }
+};
