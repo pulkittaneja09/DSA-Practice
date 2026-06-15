dbd789495e4f7eaa9e54a0c5828ca92c90cb03ab
Pulkit <pulkittaneja0509@gmail.com>
2026-06-15 17:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (63.45%) - LeetSync

---
+++ Leetcode/169-majority-element/majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int candidate = 0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                count=1;
+                candidate=nums[i];
+            }
+            else if (nums[i]==candidate) {
+                count++;
+            }
+            else{
+                count--;
+            }
+            
+        }
+        return candidate;
+    }
+};
