19f85a150328a9c30e06fb9963a2fea0f54d7438
Pulkit <pulkittaneja0509@gmail.com>
2026-08-03 12:02:18 +0530

    Time: 71 ms (42.50%) | Memory: 111.4 MB (17.62%) - LeetSync

--- Leetcode/217-contains-duplicate/contains-duplicate.cpp
+++ Leetcode/217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
+        unordered_set<int> mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])!=mp.end())
                 return true;
-            }
+            else
+                mp.insert(nums[i]);
         }
-    
-    return false;
+        return false;
     }
 };
